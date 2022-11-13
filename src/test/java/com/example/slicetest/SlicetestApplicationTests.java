1dda956092de5c43f92ee7290b17a6ac62cb50bd
minami.yoshihiko <nannany@hey.jp>
2022-11-13 16:13:25 +0900

    initial

---
+++ src/test/java/com/example/slicetest/SlicetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlicetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
